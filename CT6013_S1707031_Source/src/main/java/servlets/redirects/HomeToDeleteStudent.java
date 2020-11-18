7e74cf9d883f10502c730cb50fd157bcc2c66aa0
Denny-Jo Miller <Dennyjomiller98@aol.co.uk>
2020-11-18 15:21:25 +0000

    Teacher/Student deletion - Requires session logout

--- CT6013_S1707031_Source/src/main/java/servlets/redirects/HomeToDeleteStudent.java
+++ CT6013_S1707031_Source/src/main/java/servlets/redirects/HomeToDeleteStudent.java
@@ -17,7 +17,7 @@ public class HomeToDeleteStudent extends HttpServlet
 	static final Logger LOG = Logger.getLogger(HomeToDeleteStudent.class);
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	{
 		LOG.debug("Attempting Student Deletion");
 		String studentEmail = request.getParameter("studentEmail");
@@ -93,6 +93,7 @@ public class HomeToDeleteStudent extends HttpServlet
 		deleteStudent(dbSelection, studentBean);
 
 		//Session Attributes and redirect to HomePage
+		removeStudentSessionAttributes(request);
 		request.getSession(true).removeAttribute("editErrors");
 		request.getSession(true).setAttribute("deleteSuccess", "Student Successfully Deleted.");
 
@@ -114,6 +115,62 @@ public class HomeToDeleteStudent extends HttpServlet
 				LOG.error("Failure to redirect after student deletion redirect success", e);
 			}
 		}
+	}
+
+	private void removeStudentSessionAttributes(HttpServletRequest request)
+	{
+		request.getSession(true).removeAttribute("registrationErrors");
+		request.getSession(true).removeAttribute("loginErrors");
+		request.getSession(true).removeAttribute("firstname");
+		request.getSession(true).removeAttribute("surname");
+		request.getSession(true).removeAttribute("email");
+		request.getSession(true).removeAttribute("dob");
+		request.getSession(true).removeAttribute("address1");
+		request.getSession(true).removeAttribute("address2");
+		request.getSession(true).removeAttribute("city");
+		request.getSession(true).removeAttribute("postcode");
+		request.getSession(true).removeAttribute("pword");
+		request.getSession(true).removeAttribute("isEnrolled");
+		request.getSession(true).removeAttribute("isTeacher");
+		request.getSession(true).removeAttribute("updateSuccess");
+		request.getSession(true).removeAttribute("editErrors");
+		request.getSession(true).removeAttribute("courseSuccess");
+		request.getSession(true).removeAttribute("courseErrors");
+		request.getSession(true).removeAttribute("allCourses");
+		request.getSession(true).removeAttribute("courseCode");
+		request.getSession(true).removeAttribute("courseName");
+		request.getSession(true).removeAttribute("courseTutor");
+		request.getSession(true).removeAttribute("courseStart");
+		request.getSession(true).removeAttribute("courseEnd");
+		request.getSession(true).removeAttribute("allModules");
+		request.getSession(true).removeAttribute("moduleSuccess");
+		request.getSession(true).removeAttribute("moduleErrors");
+		request.getSession(true).removeAttribute("moduleCode");
+		request.getSession(true).removeAttribute("moduleName");
+		request.getSession(true).removeAttribute("moduleTutor");
+		request.getSession(true).removeAttribute("relatedCourse");
+		request.getSession(true).removeAttribute("semester");
+		request.getSession(true).removeAttribute("isCompulsory");
+		request.getSession(true).removeAttribute("moduleStart");
+		request.getSession(true).removeAttribute("moduleEnd");
+		request.getSession(true).removeAttribute("enrollSuccess");
+		request.getSession(true).removeAttribute("enrollErrors");
+		request.getSession(true).removeAttribute("enrollStudent");
+		request.getSession(true).removeAttribute("enrollCourse");
+		request.getSession(true).removeAttribute("enrollModules");
+		request.getSession(true).removeAttribute("markSuccess");
+		request.getSession(true).removeAttribute("markErrors");
+		request.getSession(true).removeAttribute("allMarks");
+		request.getSession(true).removeAttribute("markModule");
+		request.getSession(true).removeAttribute("markStudent");
+		request.getSession(true).removeAttribute("markTeacher");
+		request.getSession(true).removeAttribute("markGrade");
+		request.getSession(true).removeAttribute("allMarkModules");
+		request.getSession(true).removeAttribute("allMarkBeans");
+		request.getSession(true).removeAttribute("allEnrollmentToReturn");
+		request.getSession(true).removeAttribute("singleMarkBean");
+		request.getSession(true).removeAttribute("singleEnrollmentToReturn");
+		request.getSession(true).removeAttribute("deleteSuccess");
 	}
 
 	private void deleteStudentMarksAndEnrollment(String dbSelection, StudentBean studentBean)
